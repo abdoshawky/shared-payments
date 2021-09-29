2d08bbb65739c3f3f1ea02df54bfd21e55440b32
abdoshawky <abdelrhman.shawky@areebgroup.com>
2021-09-30 01:10:27 +0200

    create migrations and models

---
+++ app/Models/Payment.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'completed' => 'boolean'
+    ];
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function paidBy()
+    {
+        return $this->belongsTo(User::class, 'paid_by');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
